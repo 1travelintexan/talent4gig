300525095c688a77e4c9b7dd94b3e88e20dd7880
Joshua George <horheyinc8@gmail.com>
2021-05-28 16:02:12 +0200

    i didnt get very far

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch></Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/550?5c45f1f3bb7d0e4e25fc0c62f00013ca=5c45f1f3bb7d0e4e25fc0c62f00013ca`,
+        { withCredentials: true }
+      )
+      .then((response) => {
+        // console.log(response.data);
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>heres the page</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Movie extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default Movie;
